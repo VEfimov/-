9d84f2f0b0aa1acce5fc458799fec5c004ed6a1f
VEfimov <xzxzxd83@gmail.com>
2024-05-18 20:46:33 +0300

    Add files via upload

---
+++ prartika.py
@@ -0,0 +1,19 @@
+#print(len(str(11)))
+
+#a=2+5
+#b=2-5
+#print(len(str(a)))
+#print(len(str(b)))
+
+#a=(1+2+3)//3
+#print(str(a))
+
+
+#a='понедельник, '
+#b='вторник'
+#print(a,b)
+
+a=1
+b=2
+c=3
+print(((((a*b)+(a*c))**3)/2))
